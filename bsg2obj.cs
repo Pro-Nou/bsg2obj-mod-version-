42e9f389df2ff0188839e8a335bbc451b1cc1274
摸鱼Pro <74658051+Pro-Nou@users.noreply.github.com>
2022-06-06 14:13:29 +0800

    Add files via upload

---
+++ bsg2obj.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Modding;
+using InternalModding;
+using Modding.Common;
+using UnityEngine;
+
+namespace bsg2obj
+{
+	public class bsg2obj : ModEntryPoint
+	{
+        public static GameObject mod;
+        public override void OnLoad()
+        {
+            mod = new GameObject("bsg2obj tool");
+            UnityEngine.Object.DontDestroyOnLoad(mod);
+            mod.AddComponent<exporterControl>();
+
+            // Called when the mod is loaded.
+        }
+    }
+}
